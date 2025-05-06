a107e45ab4ed7652b3c12646bef631bf8ec1ca0e
Skylar Lea <snl.coding@gmail.com>
2025-05-06 12:01:21 -0500

    validating and preparing data; inserting data into db

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,11 +1,31 @@
 'use server';
 
+import { z } from 'zod';
+import postgres from 'postgres';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+}); //defining a schema that matches shape of form object
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true }); //schema will validate formData before saving it to db
+
 export async function createInvoice(formData: FormData) {
-    const rawFormData = {
+    const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
-      };
-      // Test it out:
-      console.log(rawFormData);    
+    });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+ 
+    await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
 }
